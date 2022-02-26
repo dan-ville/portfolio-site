06fd4311159db44c5d3b500369b241c92c00b382
Daniel Villegas <d.villegas@visiontravel.net>
2022-02-26 18:50:23 -0500

    responsive giphy

--- src/assets/GIF/GiphyEmbed.tsx
+++ src/assets/GIF/GiphyEmbed.tsx
@@ -4,14 +4,25 @@ type GiphyEmbedProps = {
 const GiphyEmbed = ({ src }: GiphyEmbedProps) => {
   return (
     <>
-      <iframe
-        title="Giphy"
-        src={src}
-        width="480"
-        height="360"
-        frameBorder="0"
-        allowFullScreen
-      />
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "400px",
+          height: "100%",
+          paddingBottom: "75%",
+          position: "relative",
+        }}
+      >
+        <iframe
+          width="100%"
+          height="300px"
+          title="Giphy"
+          style={{ position: "absolute" }}
+          frameBorder="0"
+          allowFullScreen
+          src={src}
+        ></iframe>
+      </div>
     </>
   )
 }
