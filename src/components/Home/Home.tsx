db0c5b36d2c9c9436a7adefba7bf05f961ffa6bc
Daniel Villegas <d.villegas@visiontravel.net>
2022-03-01 18:23:47 -0500

    change landing page image (kick Mack)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,17 @@
-import { useNavigate } from "react-router-dom";
-import { PageWidth, Section, StyledTitle } from "../Layout/Layout";
-import Mack from '../../assets/SVG/Mack'
+import { useNavigate } from "react-router-dom"
+import { PageWidth, Section, StyledTitle } from "../Layout/Layout"
 import {
   StyledIntro,
   StyledPitch,
-  MackIntro,
   CTA,
   CTAButton,
-} from "./Home.styles";
+  Photo,
+  PhotoWrapper,
+} from "./Home.styles"
+import profile from "../../assets/img/Profile2022.jpg"
 
 const Home = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
     <Section>
       <PageWidth>
@@ -20,17 +21,16 @@ const Home = () => {
             My name is Daniel. I love designing platforms and services that
             deliver value to people.
           </StyledPitch>
-          <Mack size="sm" />
-          <MackIntro>
-            This is Mack. He adds no SEO value but he's funny to look at.
-          </MackIntro>
+          <PhotoWrapper>
+            <Photo src={profile} />
+          </PhotoWrapper>
         </StyledIntro>
         <CTA>
           <CTAButton onClick={() => navigate("/work")}>See my work</CTAButton>
         </CTA>
       </PageWidth>
     </Section>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
